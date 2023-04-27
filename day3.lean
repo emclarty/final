5255eba62e3a9436e9b8ce3efae9b585f8364851
ShashComandur <shash@Shash-Laptop.localdomain>
2023-04-27 14:10:21 -0400

    added first function

--- day3.lean
+++ day3.lean
@@ -14,60 +14,64 @@ def break_input (s : String) : List Char × List Char :=
   let string_list := s.data
   (string_list.take (string_list.length/2), string_list.drop (string_list.length/2))
 
+def first (s : String) : Char :=
+  s.data.get! 1
+
 def match_char (s : String) : Nat :=
-  match s with
-  | "a" => 1
-  | "b" => 2
-  | "c" => 3
-  | "d" => 4
-  | "e" => 5 
-  | "f" => 6
-  | "g" => 7
-  | "h" => 8
-  | "i" => 9
-  | "j" => 10
-  | "k" => 11
-  | "l" => 12
-  | "m" => 13
-  | "n" => 14
-  | "o" => 15
-  | "p" => 16
-  | "q" => 17
-  | "r" => 18
-  | "s" => 19
-  | "t" => 20
-  | "u" => 21
-  | "v" => 22
-  | "w" => 23
-  | "x" => 24
-  | "y" => 25
-  | "z" => 26
-  | "A" => 27
-  | "B" => 28
-  | "C" => 29
-  | "D" => 30
-  | "E" => 31
-  | "F" => 32
-  | "G" => 33
-  | "H" => 34
-  | "I" => 35
-  | "J" => 36
-  | "K" => 37
-  | "L" => 38
-  | "M" => 39
-  | "N" => 40
-  | "O" => 41
-  | "P" => 42
-  | "Q" => 43
-  | "R" => 44
-  | "S" => 45
-  | "T" => 46
-  | "U" => 47
-  | "V" => 48
-  | "W" => 49
-  | "X" => 50
-  | "Y" => 51
-  | "Z" => 52
+  let f := first s
+  match f with
+  | 'a' => 1
+  | 'b' => 2
+  | 'c' => 3
+  | 'd' => 4
+  | 'e' => 5 
+  | 'f' => 6
+  | 'g' => 7
+  | 'h' => 8
+  | 'i' => 9
+  | 'j' => 10
+  | 'k' => 11
+  | 'l' => 12
+  | 'm' => 13
+  | 'n' => 14
+  | 'o' => 15
+  | 'p' => 16
+  | 'q' => 17
+  | 'r' => 18
+  | 's' => 19
+  | 't' => 20
+  | 'u' => 21
+  | 'v' => 22
+  | 'w' => 23
+  | 'x' => 24
+  | 'y' => 25
+  | 'z' => 26
+  | 'A' => 27
+  | 'B' => 28
+  | 'C' => 29
+  | 'D' => 30
+  | 'E' => 31
+  | 'F' => 32
+  | 'G' => 33
+  | 'H' => 34
+  | 'I' => 35
+  | 'J' => 36
+  | 'K' => 37
+  | 'L' => 38
+  | 'M' => 39
+  | 'N' => 40
+  | 'O' => 41
+  | 'P' => 42
+  | 'Q' => 43
+  | 'R' => 44
+  | 'S' => 45
+  | 'T' => 46
+  | 'U' => 47
+  | 'V' => 48
+  | 'W' => 49
+  | 'X' => 50
+  | 'Y' => 51
+  | 'Z' => 52
   | _ => 0
 
 def breakLines (s : String) : List String :=
@@ -108,7 +112,7 @@ def part1 : IO Nat := do
     -- if string does not exist in second, continue recursively
   -- put all responses in list
   let shared := halves.map fun (l₁, l₂) => l₁.bagInter l₂
-  
+
   -- map each shared character to its appropriate priority value
   let priorities := shared.map fun s => match_char ⟨s⟩
 
@@ -119,6 +123,7 @@ def part1 : IO Nat := do
 def part2 : IO Nat := do
   let input ← IO.FS.readFile day3_inputpath
   let shared := commonLetters input
+  IO.println (shared)
   let priorities := shared.map match_char
   return priorities.foldl (·+·) 0
 
