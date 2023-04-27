9bfe1b97034481b5ce9db14e72386aa884e1c0ca
ShashComandur <shash@Shash-Laptop.localdomain>
2023-04-27 11:07:56 -0400

    modified lakefile to require mathlib

--- lakefile.lean
+++ lakefile.lean
@@ -13,3 +13,9 @@ lean_lib «Final» {
 lean_exe «final» {
   root := `Main
 }
+
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git"@"master"
+
+meta if get_config? doc = some "on" then -- do not download and build doc-gen4 by default
+require «doc-gen4» from git "https://github.com/leanprover/doc-gen4.git" @ "main"
